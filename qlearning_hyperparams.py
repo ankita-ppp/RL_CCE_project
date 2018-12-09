50927b5f5da627346c1f73dddbb29ecfc8bcb19e
ankita-ppp <45710775+ankita-ppp@users.noreply.github.com>
2018-12-09 19:03:45 +0530

    Create qlearning_hyperparams.py

---
+++ qlearning_hyperparams.py
@@ -0,0 +1,85 @@
+##https://www.learndatasci.com/tutorials/reinforcement-q-learning-scratch-python-openai-gym/
+
+%%time
+"""Training the agent"""
+
+import random
+from IPython.display import clear_output
+
+# Hyperparameters
+alpha = 0.1
+gamma = 0.6
+epsilon = 0.1
+
+# gammas = [ 0.4, 0.5, 0.55, 0.6, 0.65, 0.7, 0.8, 0.99 ] 
+epsilons = [ 1] 
+# for gamma in gammas: 
+# epsilons = [ 0, 0.01, 0.03, 0.05, 0.08, 0.1, 0.13, 0.15, 0.2, 0.3, 1 ] 
+for epsilon in epsilons: 
+# alphas = [ 0.08, 0.1, 0.15, 0.2 ] 
+# for alpha in alphas: 
+    # For plotting metrics
+    all_epochs = []
+    all_penalties = []
+
+    for i in range(1, 100001):
+        state = env.reset()
+
+        epochs, penalties, reward, = 0, 0, 0
+        done = False
+        
+        while not done:
+            if random.uniform(0, 1) < epsilon:
+                action = env.action_space.sample() # Explore action space
+            else:
+                action = np.argmax(q_table[state]) # Exploit learned values
+
+            next_state, reward, done, info = env.step(action) 
+            
+            old_value = q_table[state, action]
+            next_max = np.max(q_table[next_state])
+            
+            new_value = (1 - alpha) * old_value + alpha * (reward + gamma * next_max)
+            q_table[state, action] = new_value
+
+            if reward == -10:
+                penalties += 1
+
+            state = next_state
+            epochs += 1
+            
+        if i % 30000 == 0:
+            clear_output(wait=True)
+            print(f"Episode: {i}")
+
+    print("Training finished.\n")
+
+    """Evaluate agent's performance after Q-learning"""
+
+    total_epochs, total_penalties = 0, 0
+    episodes = 100
+
+    for _ in range(episodes):
+        state = env.reset()
+        epochs, penalties, reward = 0, 0, 0
+        
+        done = False
+        
+        while not done:
+            action = np.argmax(q_table[state])
+            state, reward, done, info = env.step(action)
+
+            if reward == -10:
+                penalties += 1
+
+            epochs += 1
+
+        total_penalties += penalties
+        total_epochs += epochs
+
+    print(f"Results after {episodes} episodes:")
+    print(f"Results for alpha = {alpha} :")
+    print(f"Results for gamma = {gamma} :")
+    print(f"Results for epsilon = {epsilon} :")
+    print(f"Average timesteps per episode: {total_epochs / episodes}")
+    print(f"Average penalties per episode: {total_penalties / episodes}")
